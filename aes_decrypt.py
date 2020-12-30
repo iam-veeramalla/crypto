65bc1291630e40375ffebcfc4b138764a20fb4ee
iam-veeramalla <abhishek.veeramalla@gmail.com>
2020-12-30 21:14:12 +0530

    aes encryption and decryption

---
+++ aes_decrypt.py
@@ -0,0 +1,20 @@
+import base64
+import hashlib
+from Crypto.Cipher import AES
+from Crypto import Random
+
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+key = raw_input("Enter secret key ")
+
+def decrypt(enc, key):
+    private_key = hashlib.sha256(key.encode("utf-8")).digest()
+    enc = base64.b64decode(enc)
+    iv = enc[:16]
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[16:]))
+
+
+decrypted = decrypt(raw_input("Enter encrypted password"), key)
+print(bytes.decode(decrypted))
---
+++ aes_encrypt.py
@@ -0,0 +1,21 @@
+import base64
+import hashlib
+from Crypto.Cipher import AES
+from Crypto import Random
+
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+password = raw_input("Enter secret key ")
+
+
+def encrypt(raw, password):
+    private_key = hashlib.sha256(password.encode("utf-8")).digest()
+    raw = pad(raw)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(raw))
+
+encrypted = encrypt(raw_input("enter password"), password)
+print(encrypted)
